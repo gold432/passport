25faebf9c27f1351e4e54fa10d01decda70138a4
Gold Edem Hogan <edge37@outlook.com>
2024-05-05 00:20:56 +0000

    .

--- src/lib/remove_bg.ts
+++ src/lib/remove_bg.ts
@@ -1,16 +1,17 @@
+import { PUBLIC_KALEIDO } from '$env/static/public';
 import axios from 'axios';
 
-export const remove_bg = async (f) => {
+export const remove_bg = async (f: File) => {
 	const formData = new FormData();
 	formData.append('size', 'auto');
-	formData.append('image_file', fs.createReadStream(inputPath), path.basename(inputPath));
+	formData.append('image_file', f);
 	return (
 		await axios('https://api.remove.bg/v1.0/removebg', {
 			method: 'post',
 			data: formData,
 			responseType: 'arraybuffer',
 			headers: {
-				'X-Api-Key': 'INSERT_YOUR_API_KEY_HERE'
+				'X-Api-Key': PUBLIC_KALEIDO
 			}
 		})
 	).data;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,34 +2,41 @@
 	import FileUpload from '$lib/FileUpload.svelte';
 	import Teal from '../lib/Button/Teal.svelte';
 	import Passport from '../lib/Passport.svelte';
-    import html2canvas from 'html2canvas';
+	import html2canvas from 'html2canvas';
 	import '../app.css';
+	import { remove_bg } from '$lib/remove_bg';
 
-	let src: string, count = 4, top = 50, left = 50, el: HTMLDivElement;
+	let src: string = 's',
+		count = 4,
+		top = 0,
+		left = 0,
+		s = true,
+		color = '#c32222',
+		w = 132.283,
+		h = 170.079,
+		el: HTMLDivElement;
 
+	// $: position = `${left}px ${top}px`;
+	$: console.log(w);
 
-    $: position = `${left}% ${top}%`
-    $: console.log(position)
+	const download = () => {
+		// Create a canvas and set its dimensions to the element's dimensions
+		const canvas = document.createElement('canvas');
+		canvas.width = el.offsetWidth;
+		canvas.height = el.offsetHeight;
 
-    const download = () => {
+		// Use html2canvas to render the element on the canvas
+		html2canvas(el, { canvas });
 
-// Create a canvas and set its dimensions to the element's dimensions
-const canvas = document.createElement('canvas');
-canvas.width = el.offsetWidth;
-canvas.height = el.offsetHeight;
+		// Get the canvas data as a base64-encoded string
+		const imageData = canvas.toDataURL();
 
-// Use html2canvas to render the element on the canvas
-html2canvas(el, { canvas });
-
-// Get the canvas data as a base64-encoded string
-const imageData = canvas.toDataURL();
-
-// Create a link to download the image
-const link = document.createElement('a');
-link.href = imageData;
-link.download = 'image.png';
-link.click();
-    }
+		// Create a link to download the image
+		const link = document.createElement('a');
+		link.href = imageData;
+		link.download = 'image.png';
+		link.click();
+	};
 
 	const file_to_base64 = (file: File): Promise<string | ArrayBuffer | null> => {
 		return new Promise((resolve, reject) => {
@@ -46,46 +53,89 @@ link.click();
 	};
 </script>
 
-<svelte:window on:keydown={(e) => {
-    switch (e.key) {
-    case 'ArrowUp':
-      top--;
-      break;
-    case 'ArrowDown':
-      top++;
-      break;
-    case 'ArrowLeft':
-      left--;
-      break;
-    case 'ArrowRight':
-      left++;
-      break;
-  }
-}} />
+<svelte:window
+	on:keydown={(e) => {
+		switch (e.key) {
+			case 'ArrowUp':
+				top--;
+				break;
+			case 'ArrowDown':
+				top++;
+				break;
+			case 'ArrowLeft':
+				left--;
+				break;
+			case 'ArrowRight':
+				left++;
+				break;
+			case '+':
+			case 'NumpadAdd':
+				console.log('Plus key pressed');
+				break;
+			case '-':
+			case 'NumpadSubtract':
+				console.log('Minus key pressed');
+				break;
+		}
+	}}
+/>
 
 <div class="print:hidden">
-    <FileUpload on:change={async ({ detail }) => src = await file_to_base64(detail[0])} />
+	<FileUpload
+		on:change={async ({ detail }) => {
+			src = await file_to_base64(detail[0]);
+			// src = URL.createObjectURL(new Blob([await remove_bg(detail[0])], { type: 'image/png' }));
+		}}
+	/>
 
+	{#if src}
+		<Teal text="remove one" on:click={() => count--} />
+		<Teal text="add one" on:click={() => count++} />
+		<input type="color" bind:value={color} />
 
-    
-    {#if src}
-    <Teal text="remove one" on:click={() => count--} />
-        <Teal text="add one" on:click={() => count++} />
-    
-            <br />
-            <Teal text="shift left" on:click={() => left--} />
-                <Teal text="shift right" on:click={() => left++} />
-                    <Teal text="shift down" on:click={() => top++} />
-                        <Teal text="shift up" on:click={() => top--} />
-        <!-- <Teal text="Download" on:click={download} /> -->
-    {/if}
+		<!-- <Teal text="Download" on:click={download} /> -->
+	{/if}
 </div>
 
 {#if src}
-    <div bind:this={el} class="flex flex-wrap justify-between w-[385.512px] h-[574.488px] items-center bg-slate-500">
-        {#each Array(count) as e}
-    <img style="object-position: {position}" class=" border-2 border-solid border-red-400 max-w-[132.283px] max-h-[170.079px] w-[132.283px] h-[170.079px] object-cover" {src} />
-    <!-- <Passport {position} {src} /> -->
-        {/each}
-    </div>
+	<div class="flex flex-col gap-1">
+		<div
+			bind:this={el}
+			class=" flex flex-wrap justify-between w-[385.512px] h-[574.488px] items-center bg-slate-500"
+		>
+			{#each Array(count) as _}
+				<div
+					class="w-[132.283px] h-[170.079px] border-[1px] border-solid border-red-400 relative flex justify-center items-center overflow-hidden"
+				>
+					<div style=" top: {top}px; left: {left}px" class="relative w-fit h-fit">
+						<div style="width: {w}px; height: {h}px" class="flex justify-center items-center">
+							<img alt="passport with removed background" class="w-full h-auto" {src} />
+						</div>
+					</div>
+				</div>
+				<!-- <Passport {position} {src} /> -->
+			{/each}
+		</div>
+
+		<div class="print:hidden flex flex-col items-center">
+			<Teal text="shift up" on:click={() => top--} />
+			<div class="flex gap-1">
+				<Teal text="shift left" on:click={() => left--} />
+				<Teal text="shift down" on:click={() => top++} />
+				<Teal text="shift right" on:click={() => left++} />
+			</div>
+		</div>
+
+		<div class="flex gap-1">
+			<Teal text="increase width" on:click={() => w++} />
+			<Teal text="reduce width" on:click={() => w--} />
+		</div>
+
+		<radio>
+			<radiobutton>sf</radiobutton>
+		</radio>
+
+		<label for="s">full width</label>
+		<input type="checkbox" name="s" bind:checked={s} />
+	</div>
 {/if}
